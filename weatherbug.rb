3a69b8ca19ef9eefbd693a11acb46294cd80e663
Chris Craig <ccraig88@gmail.com>
2015-08-19 12:13:37 -0400

    Finished with the initial version

--- weatherbug.rb
+++ weatherbug.rb
@@ -1,16 +1,15 @@
 #!/usr/bin/env ruby
 require 'net/telnet'
 
-weatherbug = Net::Telnet::new( "Host" => "rainmaker.wunderground.com"
-  #"Port" => 23
-  #"Prompt" => "/.*[:-]$/"
-  #"Telnetmode" => false
-)
-weatherbug.waitfor(/continue:/){|c| print c}
-response = weatherbug.puts("")
-puts response
+weatherbug = Net::Telnet::new( "Host" => "rainmaker.wunderground.com" )
+weatherbug.waitfor(/continue:/)
+weatherbug.puts("")
+#puts response
 weatherbug.waitfor(/code--/)
-response = weatherbug.puts("TYS"){|c| print c}
-puts response
-weatherbug.waitfor(/X to exit:/){|c| print c}
-response = weatherbug.puts("X")
+weatherbug.puts("TYS")
+#puts response
+#response = weatherbug.waitfor(/X to exit:/){|c| puts c}
+weatherbug.waitfor(/X to exit:/){|c| puts c}
+#puts response
+#response.each_line do |line|
+weatherbug.puts("X")
