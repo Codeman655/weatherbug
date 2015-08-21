513c0c04e14cc785eecf79ffefc81c23f5b52b2a
Chris Craig <ccraig88@gmail.com>
2015-08-21 15:45:02 -0400

    small changes

--- weatherbug.rb
+++ weatherbug.rb
@@ -1,15 +1,24 @@
 #!/usr/bin/env ruby
 require 'net/telnet'
 
+if (ARGV.size != 1) then
+  puts "Usage: weatherbug airport-code"
+  exit
+end
+
 weatherbug = Net::Telnet::new( "Host" => "rainmaker.wunderground.com" )
 weatherbug.waitfor(/continue:/)
 weatherbug.puts("")
 #puts response
 weatherbug.waitfor(/code--/)
-weatherbug.puts("TYS")
+weatherbug.puts(ARGV.first.to_s)
 #puts response
-#response = weatherbug.waitfor(/X to exit:/){|c| puts c}
-weatherbug.waitfor(/X to exit:/){|c| puts c}
-#puts response
+response = weatherbug.waitfor(/X to exit:/)
+#weatherbug.waitfor(/X to exit:/){|c| puts c}
+puts response
+output = /.This (.*)\n .Tonight/.match(resopnse)
+puts output[0]
+puts output[1]
+
 #response.each_line do |line|
 weatherbug.puts("X")
