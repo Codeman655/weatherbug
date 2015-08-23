28bba023cd4c162bc6a559c86904dbf606087310
Chris Craig <ccraig88@gmail.com>
2015-08-22 23:12:23 -0400

    first version ready

--- weatherbug.rb
+++ weatherbug.rb
@@ -6,30 +6,20 @@ form="short"
 def merge_response(buffer)
   output=[] 
   start=finish=0
-  #puts "total buffer lines: " + buffer.size.to_s
   buffer.size.times do |i|
     if i < 8 then 
       next
     end
+    #Stripping newlines
     buffer[i].strip!
-    #puts i
-    #puts "line content is:"
-    #pp buffer[i].strip!
-    #puts "checking first period"
-    #puts buffer[i][0]
-    if buffer[i][0] == '.' then #TODO regex here =~ /^\./
-      #puts "set start = " + i.to_s
+    #Checking for the first '.'
+    if buffer[i][0] == '.' then 
       start=i;
     end
-    #puts "checking last period"
-    #puts buffer[i][-1]
+    #Checking for the last '.'
     if buffer[i][-1] == '.' then
       finish += 1
       #Saving in output
-      #puts "start: " + start.to_s
-      #puts "end: " + finish.to_s
-      #puts "exporting the string:"
-      #puts buffer[start,(finish-start)+1]
       output.push(buffer[start,(finish-start)+1]) #The line is complete
       next
     else #Line continues to next 
@@ -45,6 +35,7 @@ def merge_response(buffer)
   return output
 end
 
+#returning the proper Unicode character for the weather
 def parse_weather(line)
   if line =~ /\.\.\..*[sS]unny/ then
     return "\u2600"
@@ -74,15 +65,10 @@ end
 weatherbug = Net::Telnet::new( "Host" => "rainmaker.wunderground.com" )
 weatherbug.waitfor(/continue:/)
 weatherbug.puts("")
-#puts response
 weatherbug.waitfor(/code--/)
 weatherbug.puts(ARGV.first.to_s)
-#puts response
 response = weatherbug.waitfor(/X to exit:/)
 weatherbug.puts("X")
-#weatherbug.waitfor(/X to exit:/){|c| puts c}
-#puts response
-#print response.lines
 if form == "long" then
   puts response
 elsif form == "short"  then 
@@ -93,4 +79,3 @@ elsif form == "short"  then
     puts "#{md[1]}: #{char}".encode
   end
 end
-#response.each_line do |line|
